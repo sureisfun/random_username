3808a1f250b5c7d46703cb27ecb60665eb5f176d
Barry Melton <barry.melton@gmail.com>
2025-12-07 18:51:31 -0500

    moved stuff to files.

--- random_username.go
+++ random_username.go
@@ -1,10 +1,39 @@
 package random_username
 
 import (
+	_ "embed"
 	"fmt"
 	"math/rand"
 	"strings"
 )
+
+//go:embed adjectives.txt
+var adjectivesData string
+
+//go:embed nouns.txt
+var nounsData string
+
+var (
+	Adjectives []string
+	Nouns      []string
+)
+
+func init() {
+	Adjectives = parseWords(adjectivesData)
+	Nouns = parseWords(nounsData)
+}
+
+// parseWords splits embedded text data into a slice of words
+func parseWords(data string) []string {
+	lines := strings.Split(strings.TrimSpace(data), "\n")
+	words := make([]string, 0, len(lines))
+	for _, line := range lines {
+		if word := strings.TrimSpace(line); word != "" {
+			words = append(words, word)
+		}
+	}
+	return words
+}
 
 func GenerateUsername() string {
 	// Random adjective and noun
